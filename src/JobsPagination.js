4c482e229f06123698dfd654fd65bc7b7eb69313
unknown <anton13jeejo@gmail.com>
2020-08-21 04:06:01 +0530

    Initial commit

---
+++ src/JobsPagination.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Pagination } from "react-bootstrap";
+
+const JobsPagination = ({ page, setpage, hasNextPage }) => {
+  const adjustPage = (amount) => {
+    setpage((prevPage) => prevPage + amount);
+  };
+  return (
+    <Pagination>
+      {page !== 1 && <Pagination.Prev onClick={() => adjustPage(-1)} />}
+      {page !== 1 && (
+        <Pagination.Item onClick={() => setpage(1)}>1</Pagination.Item>
+      )}
+      {page > 2 && <Pagination.Ellipsis />}
+      {page > 2 && <Pagination.Item>{page - 1}</Pagination.Item>}
+      <Pagination.Item active>{page}</Pagination.Item>
+      {hasNextPage && (
+        <Pagination.Item onClick={() => adjustPage(1)}>
+          {page + 1}
+        </Pagination.Item>
+      )}
+      {hasNextPage && <Pagination.Next onClick={() => adjustPage(1)} />}
+    </Pagination>
+  );
+};
+
+export default JobsPagination;
